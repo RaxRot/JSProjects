7ccb38affaf770d968ce29237df0360948b9860e
Vladosss <dasistperfektosss@gmail.com>
2023-11-18 18:26:47 +0100

    Ready project

--- DigitalClock/script.js
+++ DigitalClock/script.js
@@ -1,4 +1,27 @@
-const hourEl = document.querySelector(".hours");
-const minutesEl = document.querySelector(".minutes");
-const secondsEl = document.querySelector(".seconds");
-const ampmEl = document.querySelector(".ampm");
+const timeElement = document.querySelector(".time");
+
+function formatTimeUnit(unit) {
+  return unit.toString().padStart(2, "0");
+}
+
+function updateClock() {
+  //Find values
+  const now = new Date();
+  let hour = formatTimeUnit(now.getHours());
+  let minutes = formatTimeUnit(now.getMinutes());
+  let seconds = formatTimeUnit(now.getSeconds());
+
+  //set values
+  let ampm = hour >= 12 ? "PM" : "AM";
+  hour = hour > 12 ? hour - 12 : hour;
+  hour = hour === 0 ? 12 : hour;
+
+  //set to user
+  const formattedTime = `${hour}:${minutes}:${seconds} ${ampm}`;
+  timeElement.textContent = formattedTime;
+
+  //check time every second
+  setTimeout(updateClock, 1000);
+}
+
+updateClock();
